01f8c3abc91097a359cb8f1bf73916ddecaf5983
Juntaroo <juliangodoy.epet20@gmail.com>
2024-06-12 21:24:32 -0300

    agregando cosas al parcial

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 import sequelize from "./db/conection.js"
 import express from "express"
+import morgan from "morgan";
+import cors from "cors";
 import routerProducts from "./routes/productosRoutes.js";
 
 const app = express();
 const port = 3000;
+
+
+app.use(morgan('dev'));
+app.use(cors());
 
 
 sequelize.authenticate();
@@ -12,7 +18,6 @@ console.log('Conexión a la base de datos establecida correctamente.');
 
 sequelize.sync({ force: true });
 console.log('Base de datos sincronizada correctamente.');
-
 
 
 app.use(express.json());
