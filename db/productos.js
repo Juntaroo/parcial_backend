775855fe316759dfe13fffd6d216360631fd4efc
Juntaroo <juliangodoy.epet20@gmail.com>
2024-06-12 21:30:55 -0300

    terminando parcial

--- db/productos.js
+++ db/productos.js
@@ -1,5 +1,6 @@
 import { DataTypes } from "sequelize";
 import sequelize from "./conection.js";
+//Utilicé como referencia el tp 3 que nos dejó
 
 const Producto = sequelize.define('Producto',{
     id: {
